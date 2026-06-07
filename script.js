8d7d7b58c678996aa4fd535f85c72fc220c85c74
linnjpr78 <linnjpr@gmail.com>
2026-06-07 15:55:41 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function updateTime() {
+    const now = new Date();
+    document.getElementById("time").innerHTML =
+        now.toLocaleString();
+}
+
+updateTime();
+
+setInterval(updateTime, 1000);
