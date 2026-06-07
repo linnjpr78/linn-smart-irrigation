6e680e423eef056fa4115b4c68c3cfe12a2673d9
linnjpr78 <linnjpr@gmail.com>
2026-06-07 16:06:14 +0530

    Update script.js
    version 2.1

--- script.js
+++ script.js
@@ -5,9 +5,14 @@ type:"line",
 data:{
 labels:["Mon","Tue","Wed","Thu","Fri","Sat","Sun"],
 datasets:[{
-label:"Temperature",
-data:[26,27,28,29,28,27,28]
+label:"Temperature (°C)",
+data:[26,27,28,29,28,27,28],
+borderWidth:3,
+fill:false
 }]
+},
+options:{
+responsive:true
 }
 }
 );
@@ -19,9 +24,21 @@ type:"line",
 data:{
 labels:["Mon","Tue","Wed","Thu","Fri","Sat","Sun"],
 datasets:[{
-label:"Humidity",
-data:[60,63,62,64,65,66,65]
+label:"Humidity (%)",
+data:[60,63,62,64,65,66,65],
+borderWidth:3,
+fill:false
 }]
+},
+options:{
+responsive:true
 }
 }
 );
+
+document.querySelector(".pump-btn")
+.addEventListener("click",function(){
+
+alert("Pump control will be connected to AWS IoT in Version 3.0");
+
+});
