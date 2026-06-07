d119748524da995d9a973ade07a009bdc0d6b47e
linnjpr78 <linnjpr@gmail.com>
2026-06-07 16:00:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-function updateTime() {
-    const now = new Date();
-    document.getElementById("time").innerHTML =
-        now.toLocaleString();
+const tempChart = new Chart(
+document.getElementById("tempChart"),
+{
+type:"line",
+data:{
+labels:["Mon","Tue","Wed","Thu","Fri","Sat","Sun"],
+datasets:[{
+label:"Temperature",
+data:[26,27,28,29,28,27,28]
+}]
 }
+}
+);
 
-updateTime();
-
-setInterval(updateTime, 1000);
+const humidityChart = new Chart(
+document.getElementById("humidityChart"),
+{
+type:"line",
+data:{
+labels:["Mon","Tue","Wed","Thu","Fri","Sat","Sun"],
+datasets:[{
+label:"Humidity",
+data:[60,63,62,64,65,66,65]
+}]
+}
+}
+);
